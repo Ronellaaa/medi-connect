a19b56c0c9b5f868f8ec4b98906d9776f34d5f07
Ronella Dias <ronelladias17@gmail.com>
2026-03-22 20:02:43 +0530

    Add microservices modules with Maven wrappers

---
+++ doctors-service/src/test/java/com/medi_connect/doctors_service/DoctorsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medi_connect.doctors_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
